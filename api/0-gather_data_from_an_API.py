45e4a3150589a3617d3a84bcd1502e1d6dac8647
Sylvieu25 <s.uwera2@alustudent.com>
2026-03-02 19:02:30 +0000

    Fix PEP8 and formatting issues in API script

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,7 +3,6 @@
 Script that returns information about an employee's TODO list progress
 using a REST API.
 """
-
 import requests
 import sys
 
@@ -12,21 +11,18 @@ def get_employee_todo_progress(employee_id):
     """Fetch and display TODO list progress for a given employee ID."""
     base_url = "https://jsonplaceholder.typicode.com"
 
-    # Get employee info
     user_url = "{}/users/{}".format(base_url, employee_id)
     user = requests.get(user_url).json()
     employee_name = user.get("name")
 
-    # Get employee todos
     todos_url = "{}/todos".format(base_url)
     todos = requests.get(todos_url, params={"userId": employee_id}).json()
 
-    # Filter completed tasks
     completed_tasks = [task for task in todos if task.get("completed") is True]
 
-    # Display progress
     print("Employee {} is done with tasks({}/{}):".format(
         employee_name, len(completed_tasks), len(todos)))
+
     for task in completed_tasks:
         print("\t {}".format(task.get("title")))
 
@@ -35,11 +31,9 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: {} <employee_id>".format(sys.argv[0]))
         sys.exit(1)
-
     try:
         employee_id = int(sys.argv[1])
         get_employee_todo_progress(employee_id)
     except ValueError:
         print("Employee ID must be an integer")
         sys.exit(1)
-
