d60280eb683aaaff9403c82cfaf112bcb80bb0bd
Sylvieu25 <s.uwera2@alustudent.com>
2026-03-02 17:56:09 +0000

    Fix formatting and logic for TODO progress script

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module fetches and displays an employee's TODO list progress
+Script that returns information about an employee's TODO list progress
 using a REST API.
 """
 
@@ -8,33 +8,38 @@ import requests
 import sys
 
 
+def get_employee_todo_progress(employee_id):
+    """Fetch and display TODO list progress for a given employee ID."""
+    base_url = "https://jsonplaceholder.typicode.com"
+
+    # Get employee info
+    user_url = "{}/users/{}".format(base_url, employee_id)
+    user = requests.get(user_url).json()
+    employee_name = user.get("name")
+
+    # Get employee todos
+    todos_url = "{}/todos".format(base_url)
+    todos = requests.get(todos_url, params={"userId": employee_id}).json()
+
+    # Filter completed tasks
+    completed_tasks = [task for task in todos if task.get("completed") is True]
+
+    # Display progress
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, len(completed_tasks), len(todos)))
+    for task in completed_tasks:
+        print("\t {}".format(task.get("title")))
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
+        print("Usage: {} <employee_id>".format(sys.argv[0]))
         sys.exit(1)
 
-    employee_id = sys.argv[1]
+    try:
+        employee_id = int(sys.argv[1])
+        get_employee_todo_progress(employee_id)
+    except ValueError:
+        print("Employee ID must be an integer")
+        sys.exit(1)
 
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-    todos_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id)
-
-    user_response = requests.get(user_url)
-    todos_response = requests.get(todos_url)
-
-    user = user_response.json()
-    todos = todos_response.json()
-
-    employee_name = user.get("name")
-    total_tasks = len(todos)
-    done_tasks = 0
-    completed_titles = []
-
-    for task in todos:
-        if task.get("completed") is True:
-            done_tasks += 1
-            completed_titles.append(task.get("title"))
-
-    print("Employee {} is done with tasks({}/{}):"
-          .format(employee_name, done_tasks, total_tasks))
-
-    for title in completed_titles:
-        print("\t {}".format(title))
