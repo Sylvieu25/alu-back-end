37bee2f105c2355d71879a9ac8c86ce7de3fc01e
Sylvieu25 <s.uwera2@alustudent.com>
2026-03-10 18:03:52 +0000

    Fixed PEP8

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,38 +1,25 @@
 #!/usr/bin/python3
-"""
-Exports employee TODO list to JSON.
-"""
-
-import json
+"""Exports employee TODO list to CSV."""
+import csv
 import requests
 import sys
 
 
 if __name__ == "__main__":
     employee_id = sys.argv[1]
-
     base_url = "https://jsonplaceholder.typicode.com"
     user_url = "{}/users/{}".format(base_url, employee_id)
     todo_url = "{}/todos?userId={}".format(base_url, employee_id)
-
     user = requests.get(user_url).json()
     todos = requests.get(todo_url).json()
-
     username = user.get("username")
-
-    # Build JSON structure
-    tasks = []
-    for task in todos:
-        tasks.append({
-            "task": task.get("title"),
-            "completed": task.get("completed"),
-            "username": username
-        })
-
-    data = {employee_id: tasks}
-
-    # Write to JSON file named USER_ID.json
-    filename = "{}.json".format(employee_id)
-    with open(filename, "w") as jsonfile:
-        json.dump(data, jsonfile)
-
+    filename = "{}.csv".format(employee_id)
+    with open(filename, "w", newline="") as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for task in todos:
+            writer.writerow([
+                employee_id,
+                username,
+                task.get("completed"),
+                task.get("title")
+            ])
