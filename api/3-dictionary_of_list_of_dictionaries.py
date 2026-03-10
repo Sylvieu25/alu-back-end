cfe1fd59426bb8cff249fa632ee9237242836909
Sylvieu25 <s.uwera2@alustudent.com>
2026-03-10 18:49:17 +0000

    Fix dictionary of list of dictionaries script

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -12,14 +12,14 @@ if __name__ == "__main__":
     for user in users:
         user_id = str(user.get("id"))
         username = user.get("username")
-        data[user_id] = [
-            {
-                "username": username,
-                "task": task.get("title"),
-                "completed": task.get("completed")
-            }
-            for task in todos
-            if str(task.get("userId")) == user_id
-        ]
+        tasks = []
+        for task in todos:
+            if str(task.get("userId")) == user_id:
+                tasks.append({
+                    "username": username,
+                    "task": task.get("title"),
+                    "completed": task.get("completed")
+                })
+        data[user_id] = tasks
     with open("todo_all_employees.json", "w") as f:
         json.dump(data, f)
