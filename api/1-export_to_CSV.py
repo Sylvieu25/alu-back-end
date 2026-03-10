a0900dd5e496e7aa58596fa41ce11774e2c10dd9
Sylvieu25 <s.uwera2@alustudent.com>
2026-03-10 17:35:05 +0000

    Fixed PEP8 line

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-Exports employee TODO list to CSV.
-"""
-
+"""Exports employee TODO list to CSV."""
 import csv
 import requests
 import sys
@@ -10,16 +7,12 @@ import sys
 
 if __name__ == "__main__":
     employee_id = sys.argv[1]
-
     base_url = "https://jsonplaceholder.typicode.com"
     user_url = "{}/users/{}".format(base_url, employee_id)
     todo_url = "{}/todos?userId={}".format(base_url, employee_id)
-
     user = requests.get(user_url).json()
     todos = requests.get(todo_url).json()
-
     username = user.get("username")
-
     filename = "{}.csv".format(employee_id)
     with open(filename, "w", newline="") as f:
         writer = csv.writer(f, quoting=csv.QUOTE_ALL)
@@ -30,4 +23,3 @@ if __name__ == "__main__":
                 task.get("completed"),
                 task.get("title")
             ])
-
