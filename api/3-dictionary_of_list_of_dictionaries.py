563aee5f40e94e4c07d6d8d649f619d2466c5c32
Sylvieu25 <s.uwera2@alustudent.com>
2026-03-10 18:40:06 +0000

    Add script to export all employees TODO list to JSON

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Exports all employees TODO list to JSON."""
+import json
+import requests
+
+
+if __name__ == "__main__":
+    base_url = "https://jsonplaceholder.typicode.com"
+    users = requests.get("{}/users".format(base_url)).json()
+    todos = requests.get("{}/todos".format(base_url)).json()
+    data = {}
+    for user in users:
+        user_id = str(user.get("id"))
+        username = user.get("username")
+        data[user_id] = [
+            {
+                "username": username,
+                "task": task.get("title"),
+                "completed": task.get("completed")
+            }
+            for task in todos
+            if str(task.get("userId")) == user_id
+        ]
+    with open("todo_all_employees.json", "w") as f:
+        json.dump(data, f)
