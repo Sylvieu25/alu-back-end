17e4e1e0c6365a76de8312ab44f335b5b5f5b3c4
Sylvieu25 <s.uwera2@alustudent.com>
2026-03-10 19:00:40 +0000

    Fix dictionary of list of dictionaries script

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,41 +1,25 @@
 #!/usr/bin/python3
-"""
-Exports all employees' TODO lists to a JSON file.
-"""
-
+"""Exports all employees TODO list to JSON."""
 import json
 import requests
 
 
 if __name__ == "__main__":
     base_url = "https://jsonplaceholder.typicode.com"
-
-    # Get all users
     users = requests.get("{}/users".format(base_url)).json()
-
-    all_tasks = {}
-
+    todos = requests.get("{}/todos".format(base_url)).json()
+    data = {}
     for user in users:
-        user_id = user.get("id")
+        user_id = str(user.get("id"))
         username = user.get("username")
-
-        # Get todos for this user
-        todos = requests.get("{}/todos".format(base_url),
-                             params={"userId": user_id}).json()
-
-        # Build list of tasks
         tasks = []
         for task in todos:
-            tasks.append({
-                "username": username,
-                "task": task.get("title"),
-                "completed": task.get("completed")
-            })
-
-        # Add to dictionary
-        all_tasks[user_id] = tasks
-
-    # Write to JSON file
-    with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump(all_tasks, jsonfile)
-
+            if str(task.get("userId")) == user_id:
+                tasks.append({
+                    "username": username,
+                    "task": task.get("title"),
+                    "completed": task.get("completed")
+                })
+        data[user_id] = tasks
+    with open("todo_all_employees.json", "w") as f:
+        json.dump(data, f)
