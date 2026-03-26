eef0ad9f00495d0d90aa75a6df18778f8c73ecab
Sylvieu25 <s.uwera2@alustudent.com>
2026-03-26 08:20:21 +0000

    Fix W293 whitespace on blank line

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         sys.exit()
-    
+
     base_url = "https://jsonplaceholder.typicode.com"
     employee_id = int(sys.argv[1])
 
